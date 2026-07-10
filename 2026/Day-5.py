ac6bbd8558fa60e3be6c9ec4b63387e149e27a5d
Jayasimha Sai Chilmakuri <chilamakurijayasimhasai@gmail.com>
2026-07-10 22:06:45 +0530

    Add Day-5.py with basic Python examples

---
+++ 2026/Day-5.py
@@ -0,0 +1,19 @@
+name = "Jayasimha"
+city = 'Hyderabad'
+print(name)
+print(city)
+
+text = "Python"
+print(text[0])
+print(text[1])
+print(text[-1])
+
+text = "Developer"
+print(text[0:4])
+print(text[4:])
+print(text[:5])
+print(text[::-1])
+
+text = "python"
+print(text.upper())
+
