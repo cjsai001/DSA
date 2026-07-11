5c0cfdbc148956e051aee25ec5a9645095a62b7e
Jayasimha Sai Chilmakuri <chilamakurijayasimhasai@gmail.com>
2026-07-11 22:18:44 +0530

    Refactor Day-6.py and add new list operations
    Removed unnecessary whitespace and added new sections for list length, traversing a list, and membership check.

--- 2026/Day-6.py
+++ 2026/Day-6.py
@@ -19,79 +19,59 @@ print(numbers)
 
 4. Add Elements
 append()
-
 Adds an element at the end.
-
 numbers = [1, 2, 3]
-
 numbers.append(4)
-
 print(numbers)
 
-Output
 
-[1, 2, 3, 4]
 insert()
 numbers = [10, 20, 40]
-
 numbers.insert(2, 30)
-
 print(numbers)
 
-Output
 
-[10, 20, 30, 40]
 extend()
 a = [1, 2]
 b = [3, 4]
-
 a.extend(b)
-
 print(a)
 
-Output
-
-[1, 2, 3, 4]
 5. Remove Elements
 remove()
 numbers = [10, 20, 30]
-
 numbers.remove(20)
-
 print(numbers)
 pop()
 numbers = [10, 20, 30]
-
 numbers.pop()
-
 print(numbers)
 del
 numbers = [10, 20, 30, 40]
-
 del numbers[1]
-
 print(numbers)
 clear()
 numbers = [10, 20]
-
 numbers.clear()
-
 print(numbers)
+
+
 6. Length
 numbers = [1, 2, 3, 4]
-
 print(len(numbers))
+
+
 7. Traversing a List
 Using for loop
 numbers = [10, 20, 30]
-
 for num in numbers:
     print(num)
 Using range()
 numbers = [10, 20, 30]
-
 for i in range(len(numbers)):
     print(numbers[i])
+
+
 8. Membership
 numbers = [5, 10, 15]
 
