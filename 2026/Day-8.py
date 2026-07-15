1efa814cb88d1858ecffae0adb9559160a24b19e
Jayasimha Sai Chilmakuri <chilamakurijayasimhasai@gmail.com>
2026-07-15 07:22:31 +0530

    Update Day-8.py

--- 2026/Day-8.py
+++ 2026/Day-8.py
@@ -1,1 +1,42 @@
+numbers = [5, 2, 8, 1, 4]
+n = len(numbers)
+for i in range(n):
+    for j in range(0, n - i - 1):
+        if numbers[j] > numbers[j + 1]:
+            numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
+print(numbers)
+
+
+numbers = [64, 25, 12, 22, 11]
+n = len(numbers)
+for i in range(n):
+    min_index = i
+    for j in range(i + 1, n):
+        if numbers[j] < numbers[min_index]:
+            min_index = j
+    numbers[i], numbers[min_index] = numbers[min_index], numbers[i]
+print(numbers)
+
+
+numbers = [12, 11, 13, 5, 6]
+for i in range(1, len(numbers)):
+    key = numbers[i]
+    j = i - 1
+    while j >= 0 and numbers[j] > key:
+        numbers[j + 1] = numbers[j]
+        j -= 1
+    numbers[j + 1] = key
+print(numbers)
+
+
+numbers = [8, 3, 1, 6]
+numbers.sort()
+print(numbers)
+numbers.sort(reverse=True)
+print(numbers)
+
+
+numbers.sort(reverse=True)
+print(numbers)
+
 
