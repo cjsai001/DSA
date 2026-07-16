b00e2e86d42f81cfd53cc6f10099107247e12041
Jayasimha Sai Chilmakuri <chilamakurijayasimhasai@gmail.com>
2026-07-16 22:28:00 +0530

    Enhance matrix operations and user input handling
    Added functionality to input a matrix, calculate sums, and print elements in various formats.

--- 2026/Day-9.py
+++ 2026/Day-9.py
@@ -11,8 +11,8 @@ matrix = [
     [1, 2, 3],
     [4, 5, 6]
 ]
-print(matrix[0][0])  # 1
-print(matrix[1][2])  # 6
+print(matrix[0][0])  
+print(matrix[1][2])  
 
 
 
@@ -27,3 +27,68 @@ for row in matrix:
     print()
 
 
+rows = int(input("Enter number of rows: "))
+cols = int(input("Enter number of columns: "))
+matrix = []
+for i in range(rows):
+    row = list(map(int, input().split()))
+    matrix.append(row)
+print("Matrix:")
+for row in matrix:
+    print(row)
+
+
+matrix = [
+    [1, 2],
+    [3, 4]
+]
+total = 0
+for row in matrix:
+    for num in row:
+        total += num
+print("Sum:", total)
+
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6]
+]
+for row in matrix:
+    print("Row Sum:", sum(row))
+
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6]
+]
+rows = len(matrix)
+cols = len(matrix[0])
+for j in range(cols):
+    total = 0
+    for i in range(rows):
+        total += matrix[i][j]
+    print("Clumn Sum:", total)
+
+
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+total = 0
+for i in range(len(matrix)):
+    total += matrix[i][i]
+print("Diagonal Sum:", total)
+
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6]
+]
+rows = len(matrix)
+cols = len(matrix[0])
+for j in range(cols):
+    for i in range(rows):
+        print(matrix[i][j], end=" ")
+    print()
