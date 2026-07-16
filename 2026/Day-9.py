03fa5a97a69c83f899b4a395fbf59e3432ea8bb5
Jayasimha Sai Chilmakuri <chilamakurijayasimhasai@gmail.com>
2026-07-16 22:23:57 +0530

    Add matrix examples and print statements in Day-9.py

---
+++ 2026/Day-9.py
@@ -0,0 +1,29 @@
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+print(matrix)
+
+
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6]
+]
+print(matrix[0][0])  # 1
+print(matrix[1][2])  # 6
+
+
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+for row in matrix:
+    for element in row:
+        print(element, end=" ")
+    print()
+
+
