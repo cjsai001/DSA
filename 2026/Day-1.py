19b64a40b25dd3eee02aed6d89148afbe3955807
Jayasimha Sai Chilmakuri <chilamakurijayasimhasai@gmail.com>
2026-07-25 23:05:29 +0530

    Enhance Day-1.py with user input and age calculation
    Added user input for name and birth year, and calculated age.

--- 2026/Day-1.py
+++ 2026/Day-1.py
@@ -13,11 +13,17 @@ print(a * b)
 n = int(input())
 print(n * n)
 print(n * n * n)
+
+
+
 print(type(10))
 print(type(7.77))
 print(type("sai"))
 print(type(True))
 print(type(False))
+
+
+
 a = 5
 b = 10
 a, b = b, a
@@ -36,6 +42,8 @@ b = int(input("enter b:"))
 print (a / b) 
 name = input("Enter your name: ")
 Birth_year = int(input("Enter your Birth year: "))
+
+
 
 print(f"Hello {name}")
 print(f"You are approximately {2025 - Birth_year} old in 2025.")
